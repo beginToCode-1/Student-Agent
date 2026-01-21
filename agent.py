7e55abb6918197dd92875b38d6d50dd8f97abdcc
beginToCode-1 <burhankhan7059@gmail.com>
2026-01-21 23:19:13 +0500

    defining new functions

--- agent.py
+++ agent.py
@@ -11,5 +11,10 @@ class StudentAgent :
 }
         self.completed = set({"Algebra", "Motion"})
         self.hours_per_day = 4
+    def add_subject(self, subject,):
+           if subject not in self.subjects:
+             self.subjects[subject] = {}
+    def add_topic(self, subject, topic, difficulty):
+              if subject in self.subjects:
+                self.subjects[subject][topic] = difficulty
 
-print("StudentAgent initialized with subjects:", StudentAgent().subjects)
