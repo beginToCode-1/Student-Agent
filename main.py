42732f49f47677ffae33a4bc746eb1e2b5524eef
beginToCode-1 <burhankhan7059@gmail.com>
2026-01-22 20:48:41 +0500

    Made an agent to think of a study plan

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from agent import StudentAgent
+from planner import generate_plan
+
 agent = StudentAgent()
 agent.add_subject("Chemistry")
-agent.add_topic("Chemistry", "Organic Chemistry", "medium")
-print("Updated subjects:", agent.subjects)
+agent.add_topic("Chemistry", "Organic Chemistry", "hard")
+agent.add_topic("Chemistry", "Inorganic Chemistry", "medium")
+
+
+
+plan = generate_plan(agent)
+
+for item in plan:
+    print(item)
