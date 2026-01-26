09670fe59c7ae48d668383218cb9b476b76c7157
beginToCode-1 <burhankhan7059@gmail.com>
2026-01-26 23:35:46 +0500

    adding a saving and loading system(json)

--- agent.py
+++ agent.py
@@ -1,7 +1,13 @@
+import json
+import os
+
+
 class StudentAgent:
-    def __init__(self):
+    def __init__(self, filename ="data/student_data.txt"):
+        self.filename = filename
         self.subjects = {}        # {subject: {topic: difficulty}}
         self.completed = set()
+        self.load_data()
 
     def add_topic(self, subject, topic, difficulty):
         if subject not in self.subjects:
@@ -18,3 +24,22 @@ class StudentAgent:
             for topic, diff in topics.items():
                 status = "DONE" if topic in self.completed else "PENDING"
                 print(f"- {subject} | {topic} | {diff} | {status}")
+
+    def save_data(self):
+        data ={
+            "subjects": self.subjects,
+            "completed": list(self.completed)
+        }
+        with open(self.filename, 'w') as f:
+            import json
+            json.dump(data, f, indent=4)
+    
+    def load_data(self):
+        if not os.path.exists(self.filname):
+            return
+        
+        with open(self.filename, 'r') as f:
+            data =json.load(f)
+            
+            self.subjects = data.get("subjects", {})
+            self.completed = set(data.get("completed", []))
